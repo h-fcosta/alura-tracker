7039e0d9b4c2d72765c8130f04d748d50aa86556
Henrique Costa <henriquefcosta19@gmail.com>
2022-08-04 18:29:38 -0300

    Projeto finalizado

---
+++ README.md
@@ -0,0 +1,2 @@
+# alura-tracker
+ Projeto do curso 'Vue3: explorando o framework' da Alura Cursos Online
---
+++ src/components/Box.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="box has-text-weight-bold" :style="estilos">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BoxVue",
+  data() {
+    return {
+      estilos: {
+        backgroundColor: "#faf0ca",
+      },
+    };
+  },
+});
+</script>
